0193abe550219bbc3ea40e61922db6804aa8b98f
jaino <msb3302@naver.com>
2023-02-19 18:22:37 +0900

    [Feature]#7 : 로그인 구현

--- client/app/src/main/java/com/example/healthc/presentation/auth/sign_in/SignInFragment.kt
+++ client/app/src/main/java/com/example/healthc/presentation/auth/sign_in/SignInFragment.kt
@@ -1,5 +1,91 @@
 package com.example.healthc.presentation.auth.sign_in
 
-class SignInFragment {
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.healthc.R
+import com.example.healthc.databinding.FragmentSignInBinding
+import com.example.healthc.domain.utils.Resource
+import com.example.healthc.presentation.auth.AuthViewModel
+import com.example.healthc.presentation.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
+@AndroidEntryPoint
+class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = checkNotNull(_binding)
+
+    private val viewModel by activityViewModels<AuthViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_in, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeData()
+        initView()
+    }
+
+    private fun observeData(){
+        viewModel.signUpEvent.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach {
+                when(it){
+                    is Resource.Loading ->{
+                        // TODO loading screen
+                    }
+                    is Resource.Success -> {
+                        startMainActivity()
+                    }
+                    is Resource.Failure -> {
+                        Toast.makeText(requireContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> {}
+                }
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+    private fun initView(){
+        binding.goToSignUpButton.setOnClickListener {
+            viewModel.initInput()
+            navigateSignUp()
+        }
+    }
+
+    private fun navigateSignUp(){
+        lifecycleScope.launchWhenStarted {
+            val direction = SignInFragmentDirections.actionSignInFragmentToSignUpNameFragment()
+            findNavController().navigate(direction)
+        }
+    }
+
+    private fun startMainActivity(){
+        val intent = Intent(requireContext(), MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
 }
