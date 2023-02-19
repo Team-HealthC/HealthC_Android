5ce610f727a435ac2485eb805b08714ff46901ae
jaino <msb3302@naver.com>
2023-02-20 00:59:30 +0900

    [Feature]#13 : UserEntity constructor, mapper 구현

--- client/app/src/main/java/com/example/healthc/data/entity/auth/UserInfoEntity.kt
+++ client/app/src/main/java/com/example/healthc/data/entity/auth/UserInfoEntity.kt
@@ -1,7 +1,21 @@
 package com.example.healthc.data.entity.auth
 
+import com.example.healthc.domain.model.auth.UserInfo
+
 data class UserInfoEntity (
     val name: String,
     val disease: List<String>,
     val allergy: List<String>,
-)
+){
+    // firebase constructor
+    constructor(): this("", emptyList(), emptyList())
+
+    // to domain model
+    fun toUserInfo() : UserInfo {
+        return UserInfo(
+            name = name,
+            disease = disease,
+            allergy = allergy
+        )
+    }
+}
