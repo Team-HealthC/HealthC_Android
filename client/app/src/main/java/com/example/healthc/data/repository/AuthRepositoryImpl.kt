2b0a908cb3c5e2e29a899a23d32bafae8e71b81a
jaino <msb3302@naver.com>
2023-02-19 19:16:57 +0900

    [Feature]#10 : 현재 회원정보 받기, 로그아웃 기능 구현

--- client/app/src/main/java/com/example/healthc/data/repository/AuthRepositoryImpl.kt
+++ client/app/src/main/java/com/example/healthc/data/repository/AuthRepositoryImpl.kt
@@ -8,12 +8,14 @@ import com.example.healthc.domain.AuthRepository
 import com.example.healthc.domain.model.auth.User
 import com.example.healthc.domain.model.auth.UserInfo
 import com.example.healthc.domain.utils.Resource
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val signInDataSource: SignInDataSource,
-    private val signUpDataSource: SignUpDataSource
+    private val signUpDataSource: SignUpDataSource,
+    private val firebaseAuth: FirebaseAuth
 ) : AuthRepository {
     override suspend fun signIn(user: User): Resource<FirebaseUser> =
         signInDataSource.signIn(UserEntity(user.email, user.password))
@@ -23,10 +25,11 @@ class AuthRepositoryImpl @Inject constructor(
             UserInfoEntity(userInfo.name, userInfo.disease, userInfo.allergy)
         )
 
+    // TODO source 구현
     override val currentUser: FirebaseUser?
-        get() = TODO("Not yet implemented")
+        get() = firebaseAuth.currentUser
 
-    override fun logout() {
-        TODO("Not yet implemented")
+    override fun signOut() {
+        firebaseAuth.signOut()
     }
 }
