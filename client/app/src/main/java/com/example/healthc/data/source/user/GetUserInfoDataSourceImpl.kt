6a76b921281b8d1718e877fd8f084d953d47f1ec
jaino <msb3302@naver.com>
2023-02-20 01:00:20 +0900

    [Feature]#13 : 회원 정보 retrieve 로직 구현

---
+++ client/app/src/main/java/com/example/healthc/data/source/user/GetUserInfoDataSourceImpl.kt
@@ -0,0 +1,27 @@
+package com.example.healthc.data.source.user
+
+import com.example.healthc.data.entity.auth.UserInfoEntity
+import com.example.healthc.data.utils.CollectionsUtil.Companion.DB_USERS
+import com.example.healthc.data.utils.await
+import com.example.healthc.domain.model.auth.UserInfo
+import com.example.healthc.domain.utils.Resource
+import com.google.firebase.firestore.FirebaseFirestore
+import javax.inject.Inject
+
+class GetUserInfoDataSourceImpl @Inject constructor(
+    private val fireStore : FirebaseFirestore
+): GetUserInfoDataSource{
+    override suspend fun getUserInfo(uid: String): Resource<UserInfo> {
+        return try{
+            val result = fireStore.collection(DB_USERS).document(uid).get().await()
+                .toObject(UserInfoEntity::class.java)
+            Resource.Success(
+                requireNotNull(result).toUserInfo()
+            )
+        }
+        catch (e : Exception){
+            e.printStackTrace()
+            Resource.Failure(e)
+        }
+    }
+}
