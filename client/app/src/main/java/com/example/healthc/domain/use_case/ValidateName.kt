e015e45950c1fcbb4b3d44da72dca99b28638384
jaino <msb3302@naver.com>
2023-02-19 18:24:09 +0900

    [Feature]#7 : 이름 유효성 검사 max length 5 -> 9

--- client/app/src/main/java/com/example/healthc/domain/use_case/ValidateName.kt
+++ client/app/src/main/java/com/example/healthc/domain/use_case/ValidateName.kt
@@ -4,10 +4,10 @@ import com.example.healthc.domain.model.validate.ValidationResult
 
 class ValidateName {
     operator fun invoke(nickName: String): ValidationResult {
-        if (nickName.length < 2) {
+        if (nickName.length !in 2..9) {
             return ValidationResult(
                 successful = false,
-                errorMessage = "이름은 2글자 이상으로 입력해주세요."
+                errorMessage = "이름은 2글자 이상 9글자 이하로 입력해주세요."
             )
         }
 
