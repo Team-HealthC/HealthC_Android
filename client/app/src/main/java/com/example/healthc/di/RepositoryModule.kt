3d02e5203b3cc738d007e448898eeff1302be8ce
jaino <msb3302@naver.com>
2023-02-20 18:11:42 +0900

    [Feature]#15 : DB 주입 설계

--- client/app/src/main/java/com/example/healthc/di/RepositoryModule.kt
+++ client/app/src/main/java/com/example/healthc/di/RepositoryModule.kt
@@ -16,7 +16,7 @@ import dagger.hilt.components.SingletonComponent
 
 @InstallIn(SingletonComponent::class)
 @Module
-class RepositoryModule {
+object RepositoryModule {
 
     @Provides
     fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
