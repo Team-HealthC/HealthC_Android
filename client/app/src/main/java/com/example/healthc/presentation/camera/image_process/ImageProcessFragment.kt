8f2b6cdd07ada5ac2c580c9e56d5e1c4f3827f5c
jaino <msb3302@naver.com>
2023-02-21 18:48:24 +0900

    [Feature]#17 : Text Recognition 구현하기

--- client/app/src/main/java/com/example/healthc/presentation/camera/image_process/ImageProcessFragment.kt
+++ client/app/src/main/java/com/example/healthc/presentation/camera/image_process/ImageProcessFragment.kt
@@ -9,13 +9,23 @@ import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.healthc.R
 import com.example.healthc.databinding.FragmentImageProcessBinding
+import com.example.healthc.presentation.camera.image_process.ImageProcessViewModel.ImageProcessEvent
+import com.example.healthc.presentation.widget.NegativeSignDialog
+import com.example.healthc.presentation.widget.PositiveSignDialog
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
+@AndroidEntryPoint
 class ImageProcessFragment : Fragment() {
 
     private var _binding: FragmentImageProcessBinding? = null
@@ -36,12 +46,17 @@ class ImageProcessFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setImage()
         recognizeText()
+        initViews()
+        observeData()
     }
 
-    private fun setImage(){
-        viewModel.setImageUrl(args.imageUrl)
+    private fun initViews(){
+        viewModel.setImageUrl(args.imageUrl) // set ImageUri
+
+        binding.backToCameraButton.setOnClickListener{
+            navigateToCamera()
+        }
     }
 
     private fun recognizeText(){
@@ -49,14 +64,46 @@ class ImageProcessFragment : Fragment() {
         val textRecognizer = TextRecognition.getClient(KoreanTextRecognizerOptions.Builder().build())
         textRecognizer.process(image)
             .addOnSuccessListener { text ->
-                viewModel.setImageText(text.text.replace("\n", ""))
+                val recognizedText = text.text.replace("\n", "")
+                viewModel.detectImage(recognizedText)
             }
             .addOnFailureListener {
                 Toast.makeText(requireContext(), "인식할 수 있는 글자가 없습니다.", Toast.LENGTH_SHORT).show()
             }
             .addOnCanceledListener {
-                Toast.makeText(requireContext(), "인식할 수 있는 글자가 없습니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "인식에 실패하였습니다. 다시 촬영 해주세요.", Toast.LENGTH_SHORT).show()
             }
+    }
+
+    private fun observeData(){
+        viewModel.imageProcessEvent.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach {
+                when(it){
+                    is ImageProcessEvent.Unit -> {}
+
+                    is ImageProcessEvent.Success -> {
+                        PositiveSignDialog(requireContext()).show()
+                    }
+
+                    is ImageProcessEvent.Detected -> {
+                        NegativeSignDialog(
+                            context = requireContext(),
+                            detectedList = it.detectedList
+                        ).show()
+                    }
+
+                    is ImageProcessEvent.Failure -> {
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }.launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+    private fun navigateToCamera(){
+        lifecycleScope.launchWhenStarted {
+            val direction = ImageProcessFragmentDirections.actionImageProcessFragmentToCameraFragment()
+            findNavController().navigate(direction)
+        }
     }
 
     override fun onDestroyView() {
--- client/app/src/main/java/com/example/healthc/presentation/camera/image_process/ImageProcessViewModel.kt
+++ client/app/src/main/java/com/example/healthc/presentation/camera/image_process/ImageProcessViewModel.kt
@@ -1,23 +1,46 @@
 package com.example.healthc.presentation.camera.image_process
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.healthc.domain.use_case.DetectText
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import timber.log.Timber
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ImageProcessViewModel: ViewModel() {
+@HiltViewModel
+class ImageProcessViewModel @Inject constructor(
+    private val detectTextUseCase : DetectText
+): ViewModel() {
 
     // 사진 저장 URL
     private val _imageUrl = MutableStateFlow("") // TODO Default image url 세팅
     val imageUrl : StateFlow<String>
         get() = _imageUrl
 
+    private val _imageProcessUiEvent = MutableStateFlow<ImageProcessEvent>(ImageProcessEvent.Unit)
+    val imageProcessEvent : StateFlow<ImageProcessEvent>
+        get() = _imageProcessUiEvent
+
     fun setImageUrl(imageUrl : String){
         _imageUrl.value = imageUrl
     }
 
-    fun onClickSavedButton(){
-        Timber.d("saved Image")
+    fun detectImage(recognizedText: String) {
+        viewModelScope.launch {
+            val result = detectTextUseCase(recognizedText)
+            if(result.successful){
+                if(result.detected) {
+                    _imageProcessUiEvent.value = ImageProcessEvent.Detected(result.detectedList)
+                }else{
+                    _imageProcessUiEvent.value = ImageProcessEvent.Success
+                }
+            }
+            else{
+                _imageProcessUiEvent.value = ImageProcessEvent.Failure("회원 정보를 가져오는데 실패하였습니다.")
+            }
+        }
     }
 
     override fun onCleared() {
@@ -25,4 +48,10 @@ class ImageProcessViewModel: ViewModel() {
         _imageUrl.value= ""
     }
 
+    sealed class ImageProcessEvent {
+        data class Detected(val detectedList: List<String>) : ImageProcessEvent()
+        data class Failure(val message : String) : ImageProcessEvent()
+        object Success : ImageProcessEvent()
+        object Unit : ImageProcessEvent()
+    }
 }
