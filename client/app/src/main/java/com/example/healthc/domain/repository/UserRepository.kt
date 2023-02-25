0b46d2dcad1d0d7dacb331266b4b4d36e7d1984e
jaino <msb3302@naver.com>
2023-02-25 18:03:14 +0900

    [Feature]#22 : repository 유저 정보 DS 호출 및 추가적인 처리 구현

--- client/app/src/main/java/com/example/healthc/domain/repository/UserRepository.kt
+++ client/app/src/main/java/com/example/healthc/domain/repository/UserRepository.kt
@@ -2,13 +2,20 @@ package com.example.healthc.domain.repository
 
 import com.example.healthc.domain.model.auth.UserInfo
 import com.example.healthc.domain.utils.Resource
+import kotlinx.coroutines.flow.Flow
 
 interface UserRepository {
 
-    // FireStore data
+    // FireStore RETRIEVE USER_INFO
     suspend fun getUserInfo(uid: String) : Resource<UserInfo>
 
+    // Firestore UPDATE USER_INFO
+    suspend fun updateUserInfo(uid: String, userInfo : UserInfo): Resource<Unit>
+
     // save data to local (sign up, sign in) TODO RETURN VALUE
-    suspend fun insertUserInfo(uid: String)
+    suspend fun updateLocalUserInfo(userInfo: UserInfo) : Resource<Unit>
+
+    // Local RETRIEVE USER_INFO
+    fun getLocalUserInfo() : Flow<Resource<UserInfo>>
 
 }
