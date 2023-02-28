ae91eee15e0ab23cb17a9d6cf747dd78855b5c02
jaino <msb3302@naver.com>
2023-02-28 21:56:29 +0900

    [Feature]#25 : SearchIngredientDataSource 구현

---
+++ client/app/src/main/java/com/example/healthc/data/source/food/SearchIngredientDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.example.healthc.data.source.food
+
+import com.example.healthc.data.remote.api.SearchFoodService
+import com.example.healthc.domain.model.food.SearchFoodIngredient
+import com.example.healthc.domain.utils.Resource
+import javax.inject.Inject
+
+class SearchIngredientDataSourceImpl @Inject constructor(
+    private val service : SearchFoodService
+) : SearchIngredientDataSource{
+    override suspend fun searchFoodMenu(element: String): Resource<SearchFoodIngredient> {
+        return try{
+            Resource.Success(
+                service.searchFoodIngredient(query = element).toSearchFoodIngredient()
+            )
+        } catch (e :Exception){
+            e.printStackTrace()
+            Resource.Failure(e)
+        }
+    }
+}
