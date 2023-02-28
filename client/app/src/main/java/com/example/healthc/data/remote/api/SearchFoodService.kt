aa627d9ef4022c788ca5c50abe8f966c4da3c3f4
jaino <msb3302@naver.com>
2023-02-28 21:55:02 +0900

    [Feature]#25 : 재료를 통해 음식 종류를 가져오는 retrofit api interface 구현

---
+++ client/app/src/main/java/com/example/healthc/data/remote/api/SearchFoodService.kt
@@ -0,0 +1,16 @@
+package com.example.healthc.data.remote.api
+
+import com.example.healthc.BuildConfig
+import com.example.healthc.data.dto.food.SearchFoodIngredientDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface SearchFoodService {
+
+    @GET("/food/ingredients/search")
+    suspend fun searchFoodIngredient(
+        @Query("apiKey") apiKey : String = BuildConfig.SPOON_API_KEY,
+        @Query("query") query : String,
+        @Query("number") number : Int = 10
+    ) : SearchFoodIngredientDto
+}
