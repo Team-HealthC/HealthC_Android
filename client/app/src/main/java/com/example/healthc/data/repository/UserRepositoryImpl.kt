77fe0c73b5ac04c96f95e9c968273c467b641acd
jaino <msb3302@naver.com>
2023-03-03 18:08:29 +0900

    [Feature]#29 : userName edit 기능 repository 추가

--- client/app/src/main/java/com/example/healthc/data/repository/UserRepositoryImpl.kt
+++ client/app/src/main/java/com/example/healthc/data/repository/UserRepositoryImpl.kt
@@ -1,20 +1,20 @@
 package com.example.healthc.data.repository
 
-import com.example.healthc.data.local.entity.UserInfoEntity
 import com.example.healthc.data.source.local.user.GetLocalUserInfoDataSource
 import com.example.healthc.data.source.local.user.UpdateLocalUserInfoDataSource
 import com.example.healthc.data.source.user.GetUserInfoDataSource
 import com.example.healthc.data.source.user.UpdateUserInfoDataSource
+import com.example.healthc.data.source.user.UpdateUserNameDataSource
 import com.example.healthc.domain.model.auth.UserInfo
 import com.example.healthc.domain.repository.UserRepository
 import com.example.healthc.domain.utils.Resource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
     private val getUserInfoDataSource: GetUserInfoDataSource,
     private val updateUserInfoDataSource: UpdateUserInfoDataSource,
+    private val updateUserNameDataSource: UpdateUserNameDataSource,
     private val updateLocalUserInfoDataSource: UpdateLocalUserInfoDataSource,
     private val getUserLocalDataSource: GetLocalUserInfoDataSource
 ) : UserRepository{
@@ -24,6 +24,10 @@ class UserRepositoryImpl @Inject constructor(
 
     override suspend fun updateUserInfo(uid : String, userInfo : UserInfo) : Resource<Unit> {
         return updateUserInfoDataSource.updateUserInfo(uid, userInfo)
+    }
+
+    override suspend fun updateUserName(uid: String, userName: String): Resource<Unit> {
+        return updateUserNameDataSource.updateUserName(uid, userName)
     }
 
     override fun getLocalUserInfo(): Flow<Resource<UserInfo>> {
