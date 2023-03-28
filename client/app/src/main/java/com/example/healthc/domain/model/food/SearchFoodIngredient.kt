c0bce22a732bbbe663fd0e17a2978ccbaab0ec81
jaino <msb3302@naver.com>
2023-03-28 14:48:47 +0900

    [Feature] #44 : Ingredient Dto, Domain 수정

--- client/app/src/main/java/com/example/healthc/domain/model/food/SearchFoodIngredient.kt
+++ client/app/src/main/java/com/example/healthc/domain/model/food/SearchFoodIngredient.kt
@@ -3,12 +3,6 @@ package com.example.healthc.domain.model.food
 data class SearchFoodIngredient(
     val number: Int,
     val offset: Int,
-    val results: List<Ingredient>,
+    val ingredients: List<String>,
     val totalResults: Int
-)
-
-data class Ingredient(
-    val id: Int,
-    val image: String,
-    val name: String
 )
