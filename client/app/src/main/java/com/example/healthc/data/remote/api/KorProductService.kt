3d4904475edf004bf19206dfb987468c83954154
jeongjaino <akgk155@gmail.com>
2023-06-16 15:18:07 +0900

    [REFACTOR] #53 : ProductService -> RecipeService, ObjectDetectionService .. 로 분할

--- client/app/src/main/java/com/example/healthc/data/remote/api/KorProductService.kt
+++ client/app/src/main/java/com/example/healthc/data/remote/api/KorProductService.kt
@@ -1,17 +1,17 @@
 package com.example.healthc.data.remote.api
 
 import com.example.healthc.BuildConfig
-import com.example.healthc.data.dto.food.ingredient.SearchFoodProductDto
+import com.example.healthc.data.dto.kor_product.KorProductResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface SearchFoodProductService {
+interface KorProductService {
 
     @GET("/B553748/CertImgListService/getCertImgListService")
-    suspend fun searchFoodProduct(
+    suspend fun getKorProduct(
         @Query("ServiceKey") ServiceKey : String = BuildConfig.DATA_GO_PRODUCT_API_KEY,
         @Query("returnType") returnType : String = "json",
         @Query("prdlstNm") prdlstNm : String
-    ) : SearchFoodProductDto
+    ) : KorProductResponse
 
 }
