045ce8549e437f28d15b77209db9c0d475b7429c
jeongjaino <akgk155@gmail.com>
2023-08-27 22:18:11 +0900

    [CHORE] #65 : rename UserDto -> UserResponse

---
+++ client/app/src/main/java/com/example/healthc/data/model/remote/auth/UserResponse.kt
@@ -0,0 +1,13 @@
+package com.example.healthc.data.model.remote.auth
+
+import com.example.healthc.domain.model.auth.User
+
+data class UserResponse(
+    val name: String,
+    val allergies: List<String>
+){
+    fun toDomain() = User(
+        name = name,
+        allergies = allergies
+    )
+}
