53fbff7d0a6cb2697ebbdae2b90bc80a5dcfb012
jeongjaino <akgk155@gmail.com>
2023-08-28 23:57:18 +0900

    [REFACTOR] #65 : (product) repository -> usecase 메소드 변경, UiEvent 변경, Resource -> Result 메소드 변경

--- client/app/src/main/java/com/example/healthc/presentation/product/product/product_detail/ProductViewModel.kt
+++ client/app/src/main/java/com/example/healthc/presentation/product/product/product_detail/ProductViewModel.kt
@@ -4,17 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.healthc.domain.model.product.NutritionLabel
 import com.example.healthc.domain.model.product.Product
-import com.example.healthc.domain.repository.ProductRepository
-import com.example.healthc.domain.utils.Resource
+import com.example.healthc.domain.usecase.product.GetNutritionLabelUseCase
+import com.example.healthc.domain.usecase.product.GetProductUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductViewModel @Inject constructor(
-    private val repository : ProductRepository
+    private val getProductUseCase: GetProductUseCase,
+    private val getNutritionLabelUseCase: GetNutritionLabelUseCase
 ): ViewModel() {
 
     private val _productUiState : MutableStateFlow<Product> = MutableStateFlow(Product())
@@ -23,43 +26,35 @@ class ProductViewModel @Inject constructor(
     private val _productFact : MutableStateFlow<NutritionLabel> = MutableStateFlow(NutritionLabel())
     val productFact : StateFlow<NutritionLabel> get() = _productFact
 
-    private val _productInfoUiEvent = MutableStateFlow<UiEvent>(UiEvent.Unit)
-    val productInfoUiEvent get() = _productInfoUiEvent
+    private val _productEvent = MutableSharedFlow<ProductEvent>()
+    val productEvent: SharedFlow<ProductEvent> get() = _productEvent
 
     fun getProductInfo(id : Int){
         viewModelScope.launch {
-            val result = repository.getProduct(id)
-            when(result){
-                is Resource.Success -> {
-                    _productUiState.value = result.result
+            getProductUseCase(id)
+                .onSuccess {  product ->
+                    _productUiState.value = product
                 }
-                is Resource.Failure -> {
-                    _productInfoUiEvent.value = UiEvent.Failure("상품 정보를 불러오는데 실패하였습니다.")
+                .onFailure { error ->
+                    _productEvent.emit(ProductEvent.Failure(error.message))
                 }
-                is Resource.Loading -> {
-
-                }
-            }
         }
     }
 
     fun getProductFact(id : Int){
         viewModelScope.launch {
-            val result = repository.getNutritionLabel(id)
-            when(result){
-                is Resource.Success -> {
-                    _productFact.value = result.result
+            getNutritionLabelUseCase(id)
+                .onSuccess {  label ->
+                    _productFact.value = label
                 }
-                is Resource.Failure -> {
-                    _productInfoUiEvent.value = UiEvent.ImageFailure("영양성분표를 들고오는데 실패하였습니다.")
+                .onFailure { error ->
+                    _productEvent.emit(ProductEvent.ImageFailure(error.message))
                 }
-                is Resource.Loading -> {}
-            }
         }
     }
-    sealed class UiEvent{
-        data class Failure(val message : String?) : UiEvent()
-        data class ImageFailure(val message : String?) : UiEvent()
-        object Unit : UiEvent()
+
+    sealed class ProductEvent{
+        data class Failure(val message : String?) : ProductEvent()
+        data class ImageFailure(val message : String?) : ProductEvent()
     }
 }
