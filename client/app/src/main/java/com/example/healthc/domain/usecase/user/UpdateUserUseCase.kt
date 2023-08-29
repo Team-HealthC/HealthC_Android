0cde9996b62cb3759acfb3a435e71476498f1329
jeongjaino <akgk155@gmail.com>
2023-08-29 20:20:40 +0900

    [REFACTOR] #66 : request로 domain user model 사용하는 로직 소거

--- client/app/src/main/java/com/example/healthc/domain/usecase/user/UpdateUserUseCase.kt
+++ client/app/src/main/java/com/example/healthc/domain/usecase/user/UpdateUserUseCase.kt
@@ -1,15 +1,12 @@
 package com.example.healthc.domain.usecase.user
 
-import com.example.healthc.domain.model.auth.User
 import com.example.healthc.domain.repository.UserRepository
 import javax.inject.Inject
 
 class UpdateUserUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(userName: String, allergies: List<String>): Result<Unit> {
-        return userRepository.updateUser(
-            User(userName, allergies)
-        )
+    suspend operator fun invoke(name: String, allergies: List<String>): Result<Unit> {
+        return userRepository.updateUser(name, allergies)
     }
 }
