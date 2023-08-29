b5d8493f067391dc17edb05d6a3cbc809c3b94f4
jeongjaino <akgk155@gmail.com>
2023-08-29 20:33:48 +0900

    [FIX] #66 : 사용자 알러지 요소와 음식 알러지 요소 비교할 때, 빠진 번역 로직 추가하기

--- client/app/src/main/java/com/example/healthc/domain/usecase/detection/CheckAllergiesInImageUseCase.kt
+++ client/app/src/main/java/com/example/healthc/domain/usecase/detection/CheckAllergiesInImageUseCase.kt
@@ -1,7 +1,6 @@
 package com.example.healthc.domain.usecase.detection
 
-import com.example.healthc.domain.model.recipe.Ingredient
-import com.example.healthc.domain.repository.DetectionRepository
+import com.example.healthc.domain.model.auth.Allergy
 import com.example.healthc.domain.repository.RecipeRepository
 import com.example.healthc.domain.repository.UserRepository
 import javax.inject.Inject
@@ -10,16 +9,17 @@ class CheckAllergiesInImageUseCase @Inject constructor(
     private val userRepository: UserRepository,
     private val recipeRepository: RecipeRepository
 ){
-    suspend operator fun invoke(detectedObject: String): Result<List<String>> {
+    suspend operator fun invoke(detectedObject: String): Result<List<Allergy>> {
         return runCatching {
             val ingredientList = recipeRepository.getIngredientList(detectedObject).getOrThrow()
             val user = userRepository.getUser().getOrThrow()
 
-            val detectedAllergies = mutableListOf<String>()
+            val detectedAllergies = mutableListOf<Allergy>()
             user.allergies.forEach { allergy ->
                 val ingredientNameList = ingredientList.map{ it.name }
+                val translatedAllergy = allergy.toEnglish().toString()
 
-                if(ingredientNameList.contains(allergy)){
+                if(ingredientNameList.contains(translatedAllergy)){
                     detectedAllergies.add(allergy)
                 }
             }
