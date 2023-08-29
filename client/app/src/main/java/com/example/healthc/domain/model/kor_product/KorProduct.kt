e852d60eb34fbfe4a052d1f56cbf6949b472d844
jeongjaino <akgk155@gmail.com>
2023-08-29 23:08:46 +0900

    [CHORE] #50 : Domain KorProduct Model에 id 필드 추가

--- client/app/src/main/java/com/example/healthc/domain/model/kor_product/KorProduct.kt
+++ client/app/src/main/java/com/example/healthc/domain/model/kor_product/KorProduct.kt
@@ -1,10 +1,21 @@
 package com.example.healthc.domain.model.kor_product
 
 data class KorProduct(
+    val id: String,
     val allergies: String, // 알러지 유발 물질
     val image: String, // 이미지 소스
     val nutrient: String, // 영양
     val category: String, // 제품 카테고리
     val name: String, // 제품 이름
     val rawMaterials : String, // 원재료 구성
-)
+){
+    constructor(): this(
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    )
+}
