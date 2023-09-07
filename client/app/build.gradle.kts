bd182336474ae59c0fc0a3821183842e8a883efb
jeongjaino <akgk155@gmail.com>
2023-09-07 16:02:59 +0900

    [FEATURE] #72 : custom plugin 적용

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -1,36 +1,24 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.google.services)
-    alias(libs.plugins.google.firebase.crashlytics)
-    alias(libs.plugins.kotlin.android)
+    id("com.healthc.application")
+    id("com.healthc.hilt")
+    id("com.healthc.kotlin")
+    id("com.healthc.google.services")
+    id("com.healthc.navigation")
     alias(libs.plugins.kotlin.kapt)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.dagger.hilt)
-    alias(libs.plugins.androidx.navigation.safeargs)
 }
 
 android {
-    namespace = "com.example.healthc"
-    compileSdk = libs.versions.compileSdk.get().toInt()
+    namespace = "com.healthc.app"
 
     defaultConfig {
         applicationId = "com.example.healthc"
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = libs.versions.versionCode.get().toInt()
-        versionName = libs.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        // Local Properties
-        buildConfigField("String", "SPOON_API_KEY",  getLocalProperty("SPOON_API_KEY"))
-        buildConfigField("String", "SPOON_BASE_URL",  getLocalProperty("SPOON_BASE_URL"))
         buildConfigField("String", "SPOON_API_INGREDIENT_URL", getLocalProperty("SPOON_API_INGREDIENT_URL"))
         buildConfigField("String", "SPOON_API_PRODUCT_URL", getLocalProperty("SPOON_API_PRODUCT_URL"))
-        buildConfigField("String", "DATA_GO_PRODUCT_BASE_URL", getLocalProperty("DATA_GO_PRODUCT_BASE_URL"))
-        buildConfigField("String", "DATA_GO_PRODUCT_API_KEY", getLocalProperty("DATA_GO_PRODUCT_API_KEY"))
     }
 
     buildTypes {
@@ -39,21 +27,6 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
-    buildFeatures {
-        buildConfig = true
-        dataBinding = true
-        viewBinding = true
-    }
 }
 
 fun getLocalProperty(property: String): String {
@@ -61,31 +34,16 @@ fun getLocalProperty(property: String): String {
 }
 
 dependencies {
+    implementation(project(":domain"))
+    implementation(project(":data"))
+
     // androidX
     implementation(libs.bundles.androidx)
-    implementation(libs.bundles.androidx.navigation)
     implementation(libs.bundles.androidx.camerax)
     implementation(libs.androidx.splash)
 
-    // kotlin & coroutine
-    implementation(libs.kotlin)
-    implementation(libs.kotlin.coroutines)
-
-    // firebase
-    implementation(platform(libs.google.firebase))
-    implementation(libs.bundles.firebase)
-
-    // material & text recognition
+    // material
     implementation(libs.google.material)
-    implementation(libs.bundles.text.recognition)
-
-    // retrofit & okhttp
-    implementation(libs.bundles.retrofit)
-    implementation(libs.bundles.okhttp)
-
-    // hilt & compiler
-    implementation(libs.hilt)
-    ksp(libs.hilt.ksp)
 
     // timber, glide, systemUiController
     implementation(libs.timber)
