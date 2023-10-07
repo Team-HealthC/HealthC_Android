848ab70dc58ad1e4e1826427dd7c8ee55bcdda51
jeongjaino <akgk155@gmail.com>
2023-10-07 16:23:19 +0900

    [STYLE] #89 : Plugin 임포트 수정

--- client/build-logic/convention/src/main/java/com/healthc/plugin/AndroidApplicationPlugin.kt
+++ client/build-logic/convention/src/main/java/com/healthc/plugin/AndroidApplicationPlugin.kt
@@ -1,15 +1,10 @@
 package com.healthc.plugin
 
 import com.android.build.gradle.BaseExtension
-import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.plugins.ExtensionAware
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
 
 class AndroidApplicationPlugin: Plugin<Project>{
     override fun apply(target: Project) {
